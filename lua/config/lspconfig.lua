7d1930b244da838a373079a6d037cd9d00987635
Jeremy Kettlewell <jkettlewell@tentcraft.com>
2024-08-26 10:02:29 -0400

    Added atac and github plugins

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,4 +1,6 @@
-require("lspconfig").phpactor.setup({
+local nvim_lsp = require("lspconfig")
+
+nvim_lsp.phpactor.setup({
   root_dir = function(_)
     return vim.fn.getcwd()
   end,
@@ -6,9 +8,8 @@ require("lspconfig").phpactor.setup({
     ["language_server_phpstan.enabled"] = false,
     ["language_server_psalm.enabled"] = false,
     ["language_server_completion.autoimport"] = true, -- Enable auto-import on completion
-    ["language_server.diagnostics_on_save"] = true,  -- Run diagnostics on save
-    ["language_server.indexing.enabled"] = true,  -- Enable background indexing
-    ["language_server_completion.resolve_use_statements"] = true,  -- Resolve use statements for completion
-
+    ["language_server.diagnostics_on_save"] = true, -- Run diagnostics on save
+    ["language_server.indexing.enabled"] = true, -- Enable background indexing
+    ["language_server_completion.resolve_use_statements"] = true, -- Resolve use statements for completion
   },
 })
