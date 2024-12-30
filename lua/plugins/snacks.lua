3ff70556ecca26954081e995261e9f1cd995f6fc
Jeremy Kettlewell <jkettlewell@tentcraft.com>
2024-12-29 20:07:54 -0500

    Update plugins and added avante

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = { example = "compact_files" },
+    },
+    keys = {
+      {
+        "<leader>.",
+        function()
+          Snacks.scratch()
+        end,
+        desc = "Toggle Scratch Buffer",
+      },
+      {
+        "<leader>S",
+        function()
+          Snacks.scratch.select()
+        end,
+        desc = "Select Scratch Buffer",
+      },
+    },
+  },
+}
